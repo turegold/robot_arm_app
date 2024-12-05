6ab4cc8b52572d8740f8c03aefadc33e38df7bb6
Truegold <[bacaren21@naver.com]>
2024-12-05 16:04:56 +0900

    날씨 UI 업데이트

--- App.js
+++ App.js
@@ -7,23 +7,21 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>결과 : {number}</Text>
-      <View style={styles.btn_Group}>
-        <Button
-          style={styles.btn}
-          title="증가"
-          onPress={() => {
-            setNumber(number + 1);
-          }}
-        />
-        <Button
-          style={styles.btn}
-          title="감소"
-          onPress={() => {
-            setNumber(number - 1);
-          }}
-        />
+      <View style={styles.cityContainer}>
+        <Text style={styles.city}>Incheon</Text>
       </View>
+
+      <View style={styles.weatherContainer}>
+        <View style={styles.day}>
+          <Text style={styles.regDate}>12월 5일, 목, 15:49</Text>
+          <Text style={styles.weather}>맑음</Text>
+        </View>
+
+        <View style={styles.tempContainer}>
+          <Text style={styles.temp}>15</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -32,16 +30,61 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ffe01a",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  cityContainer: {
+    flex: 1,
+  },
+  city: {
+    flex: 1,
+    marginTop: 50,
+    paddingTop: 20,
+    fontSize: 40,
+    textAlign: "center",
+    fontWeight: "bold",
+    // backgroundColor: "red",
+  },
+  weatherContainer: {
+    flex: 3,
+    // backgroundColor: "green",
+  },
+  day: {
+    flex: 0.2,
+    // backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    fontSize: 50,
+  regDate: {
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
     fontWeight: "bold",
+    borderRadius: 20,
+    overflow: "hidden",
+    flex: 1,
+    color: "white",
+    backgroundColor: "black",
   },
-  btn_Group: {
-    flexDirection: "row",
-    gap: 10,
+  weather: {
+    flex: 1.5,
+    marginTop: 20,
+    fontSize: 25,
+    fontWeight: "bold",
+    // backgroundColor: "purple",
+  },
+  tempContainer: {
+    flex: 0.3,
+    alignItems: "center",
+    // backgroundColor: "blue",
+    justifyContent: "center",
+  },
+  temp: {
+    flex: 0.5,
+    fontSize: 90,
+    textAlign: "center",
+    fontWeight: "bold",
+    // backgroundColor: "orange",
   },
 });
